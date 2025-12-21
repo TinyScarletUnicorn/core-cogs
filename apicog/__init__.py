7df2e36f080cf3ab4bad5156138ff8feed073ee4
AA <chasehult@gmail.com>
2025-12-21 15:00:38 -0800

    Add new (better) API cog

---
+++ apicog/__init__.py
@@ -0,0 +1,11 @@
+import json
+from pathlib import Path
+
+from apicog.apicog import APICog
+
+with open(Path(__file__).parent / "info.json") as file:
+    __red_end_user_data_statement__ = json.load(file)['end_user_data_statement']
+
+
+async def setup(bot):
+    await bot.add_cog(APICog(bot))
